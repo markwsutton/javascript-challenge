5299c0b125989d712d10a5d1917811e13fc8a0ac
markwsutton <64506426+markwsutton@users.noreply.github.com>
2020-08-29 17:20:14 -0700

    added code to display table data from the data.js file

--- static/js/app.js
+++ static/js/app.js
@@ -2,3 +2,24 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+// get all table attributes
+var tbody = d3.select("tbody");
+
+function buildTable(data){
+    //Make sure table is empty
+    tbody.html("");
+
+    // loop through and append data
+    data.forEach(function(dataRow){
+        var row = tbody.append("tr");
+
+        // loop hrough rows add values into cells
+        Object.values(dataRow).forEach(function(val){
+            var cell = row.append("td");
+            cell.text(val);
+        })
+    })
+
+}
+
+buildTable(tableData);
